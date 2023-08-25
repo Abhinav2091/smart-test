ed65093bdf0d1a67918c4fcd480992d4d3d5d0c3
Abhinav2091 <abhinavsharma043@gmail.com>
2023-08-26 02:22:20 +0530

    initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # smart-test
+Spring boot version - 3.1.3
+java version 17
---
+++ src/test/java/com/smart/test/smarttest/SmartTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.test.smarttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
