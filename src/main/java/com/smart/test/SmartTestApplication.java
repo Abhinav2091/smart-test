5b0ec305b44c98a13834d6eca4cf434ca4cddc3e
Abhinav2091 <abhinavsharma043@gmail.com>
2023-08-27 23:09:51 +0530

    logs for jenkines

--- src/main/java/com/smart/test/SmartTestApplication.java
+++ src/main/java/com/smart/test/SmartTestApplication.java
@@ -1,13 +1,24 @@
 package com.smart.test;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartTestApplication.class, args);
-	}
+    public static final Logger LOGGER = LoggerFactory.getLogger(SmartTestApplication.class);
+
+    @PostConstruct
+    public void init() {
+		LOGGER.info("Application started");
+    }
+
+    public static void main(String[] args) {
+		LOGGER.info("Application executed");
+        SpringApplication.run(SmartTestApplication.class, args);
+    }
 
 }
--- src/test/java/com/smart/test/SmartTestApplicationTests.java
+++ src/test/java/com/smart/test/SmartTestApplicationTests.java
@@ -1,13 +1,18 @@
 package com.smart.test;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmartTestApplicationTests {
+	public static final Logger LOGGER = LoggerFactory.getLogger(SmartTestApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		LOGGER.info("inside test");
+		assert true;
 	}
 
 }
